1af2fd9e58d13674ab36e24dfbf55bf1a302d92c
MedMax-76 <maximusmax76@gmail.com>
2024-02-19 22:42:27 +0200

    #

--- src/moduls/Nav.js
+++ src/moduls/Nav.js
@@ -1,4 +1,5 @@
-import logo from "../img/Logo_black.svg";
+// import logo from "../img/Logo_black.svg";
+import logo from "../img/Logo_1 1.svg";
 
 const Nav = () => {
   return (
@@ -9,6 +10,9 @@ const Nav = () => {
             <a href="/">
               <img className="nav-row-logo-img" src={logo} alt="Logp Maks" />
             </a>
+            {/* <a href="/">
+              <img className="nav-row-logo-img" src={logo} alt="Logp Maks" />
+            </a> */}
             <ul className="nav-list">
               <li className="nav-list__item">
                 <a href="contacts" className="nav-list__link">
